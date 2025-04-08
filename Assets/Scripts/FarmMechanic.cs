b2085ce488f3d7e747d2a5bd4ea92db1ab09c53f
Luca Calmera <acacianbench@gmail.com>
2025-04-08 11:41:46 +0200

    Add Watering to FarmMechanic
    Small addition of a watering function inside of FarmMechanic

--- Assets/Scripts/FarmMechanic.cs
+++ Assets/Scripts/FarmMechanic.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FarmMechanic : MonoBehaviour
 {
-    [SerializeField] private float m_timeToGrowMultiplier; //optional multiplier for growing the crop to work with timing
     [SerializeField] private float m_timeToGrow; //The time for a crop to grow, can be later changed to the day-night cycle internal clock
+    [SerializeField] private float m_wateringMultiplier;
 
     private bool startGrowing = false; //Bool to check if plant is growing
     private bool growingComplete = false; //Bool to check if plant growth is complete
@@ -28,7 +28,7 @@ public class FarmMechanic : MonoBehaviour
 
     private void Update() //Growing phrase
     {
-        if (Input.GetKeyDown("p")) //When "p" is pressed, planting will begin
+        if (Input.GetKeyDown("p")) //When "P" is pressed, planting will begin
         {
             Plant();
         }
@@ -43,13 +43,24 @@ public class FarmMechanic : MonoBehaviour
             }
         }
 
+        if (Input.GetKeyDown("w")) //When "W" is pressed, it will water the plant and make it grow faster
+        {
+            float rand = UnityEngine.Random.Range(0.1f, 0.2f); //random amount of effect added on thanks to watering
+            m_wateringMultiplier += rand;
+            if (m_wateringMultiplier > 1.1f)
+            {
+                m_wateringMultiplier = 1.0f;
+            }
+        }
+
         if (startGrowing) //The growing process
         {
             if (!growingComplete)
             {
                 float timeToGrow = m_timeToGrow;
-                float growPosition = processOfGrowth * m_timeToGrowMultiplier;
-                gameObject.transform.localPosition = new Vector3(0, 0.2f - growPosition, 0); //0.2 is the starting position minus the growPosition to make it go up
+                float growPosition = processOfGrowth * 0.1f;
+                float watering = m_wateringMultiplier;
+                gameObject.transform.localPosition = new Vector3(0, 0.2f * watering - growPosition, 0); //0.2 is the starting position minus the growPosition to make it go up
                 processOfGrowth -= 1.0f / timeToGrow;
             }
         }

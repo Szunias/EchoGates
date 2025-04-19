977d70dc6a592c54c83788647fa6162c17cdc38d
Daphny Huys <152285538+Daphny-Huys@users.noreply.github.com>
2025-04-19 12:43:49 +0200

    Feat Stop Movement/CameraRotation in Inventory

--- Assets/Scripts/CamerMovement.cs
+++ Assets/Scripts/CamerMovement.cs
@@ -8,19 +8,26 @@ public class CameraMovement : MonoBehaviour
     public float lookXLimit = 45f;
     public float lookSpeed = 2f;
 
+    private bool shouldRotate = true;
+
     float inputX;
     float inputY;
     private void Update()
     {
-        inputX += Input.GetAxis("Mouse X") * lookSpeed;
-        inputY += Input.GetAxis("Mouse Y") * lookSpeed;
-        inputY = Mathf.Clamp(inputY, -lookXLimit, lookXLimit);
+        if (shouldRotate)
+        {
+            inputX += Input.GetAxis("Mouse X") * lookSpeed;
+            inputY += Input.GetAxis("Mouse Y") * lookSpeed;
+            inputY = Mathf.Clamp(inputY, -lookXLimit, lookXLimit);
 
 
+            Quaternion rotation = Quaternion.Euler(-inputY, inputX, 0);
+            transform.rotation = rotation;
+        }
 
-        Quaternion rotation = Quaternion.Euler(-inputY, inputX, 0);
-        transform.rotation = rotation;
-
-
+    }
+    public void EnableRotation(bool enable)
+    {
+        shouldRotate = enable;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,37 +5,45 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 16;
     [SerializeField] private GameObject inventoryObject;
+    [SerializeField] private GameObject cameraObject;
 
     private Vector3 spawnPoint;
     private CharacterController characterController;
     private new Camera camera;
     private bool inInventory;
     private Canvas inventoryCanvas;
+    private CameraMovement cameraComponent;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
         spawnPoint = transform.position;
         camera = Camera.main;
+
         inventoryCanvas = inventoryObject.GetComponent<Canvas>();
         inventoryCanvas.enabled = false;
+
+        cameraComponent = cameraObject.GetComponent<CameraMovement>();
     }
 
     private void Update()
     {
+        if (!inInventory) 
+        {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            Vector3 forward = camera.transform.forward;
+            Vector3 right = camera.transform.right;
 
-        Vector3 forward = camera.transform.forward;
-        Vector3 right = camera.transform.right;
+            forward.y = 0f;
+            right.y = 0f;
 
-        forward.y = 0f;
-        right.y = 0f;
-
-        Vector3 direction = forward * input.z + right * input.x;
+            Vector3 direction = forward * input.z + right * input.x;
 
 
-        characterController.Move(direction * moveSpeed * Time.deltaTime);
+            characterController.Move(direction * moveSpeed * Time.deltaTime);
+        }
+        
     }
 
     public void ResetPosition()
@@ -52,7 +60,10 @@ public class PlayerMovement : MonoBehaviour
             inInventory = !inInventory;
 
             inventoryCanvas.enabled = inInventory;
+
+            cameraComponent.EnableRotation(!inInventory);
         }
     }
+    
 }
 

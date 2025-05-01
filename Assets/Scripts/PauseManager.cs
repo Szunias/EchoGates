f08f4976fc4482ee835e062692cb0a1a85e32dd7
FleurLievens <fleur.lievens1@gmail.com>
2025-05-01 15:24:45 +0200

    pausing game when inventory is opened

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public bool IsPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (IsPaused)
+                ClosePauseMenu();
+            else
+                OpenPauseMenu();
+        }
+    }
+
+    public void OpenPauseMenu()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;  // Pause all time-based activity (animations, physics, etc.)
+        IsPaused = true;
+    }
+    public void ClosePauseMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;  // Resume normal gameplay
+        IsPaused = false;
+                              
+      
+    }  
+}

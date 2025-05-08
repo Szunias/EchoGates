804dfc157b968c6456343cd8cbd29e7f12eb9ae8
Isabelle044 <isabelle04@live.nl>
2025-05-08 20:43:48 +0200

    Attemt to combine scripts
    i tried to combine the left and right mouseclick scrit and failed

--- Assets/Scripts/CubeLight.cs
+++ Assets/Scripts/CubeLight.cs
@@ -1,9 +1,13 @@
 using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Light))]
+[RequireComponent(typeof(LineRenderer))] 
 public class CubeLight : MonoBehaviour
 {
+    public Camera playerCamera;
+    public Transform beamOrigin;
     public int MouseButton = 1; //for the right mouse button
 
     [Header("Energy Settings")]
@@ -16,12 +20,20 @@ public class CubeLight : MonoBehaviour
     public float MaxIntensity = 2f; // Brightness at 100%
     public float LightDuration = 4f; // Light stays on for this duration
 
+    [Tooltip("Range of the beam")][SerializeField] public float soulRange = 50f;
+    [Tooltip("Time between beam shots")][SerializeField] public float fireRate = 0.2f;
+    [Tooltip("How long the soul activates its beam")][SerializeField] public float beamDuration = 0.05f; 
+
     private Light LightCube;
     private float LightTimer = 0f;
     private bool IsCasting = false;
 
+    LineRenderer beamLine;
+    float fireTimer;
+
     void Start()
     {
+        beamLine = GetComponent<LineRenderer>();
         LightCube = GetComponent<Light>();
         LightCube.type = LightType.Point;
         LightCube.enabled = false;
@@ -44,6 +56,32 @@ public class CubeLight : MonoBehaviour
                 LightCube.enabled = true;
             }
         }
+
+        fireTimer += Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && fireTimer > fireRate) //Fire1 is equal to right click
+        {
+            fireTimer = 0;
+            beamLine.SetPosition(0, beamOrigin.position);
+            Vector3 rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, soulRange))
+            {
+                beamLine.SetPosition(1, hit.point);
+                Destroy(hit.transform.gameObject); //can be changed to whatever we want to do when beam hits
+            }
+            else
+            {
+                beamLine.SetPosition(1, rayOrigin + (playerCamera.transform.forward * soulRange));
+            }
+            StartCoroutine(ShootBeam());
+        }
+
+        IEnumerator ShootBeam()
+        {
+            beamLine.enabled = true;
+            yield return new WaitForSeconds(beamDuration);
+            beamLine.enabled = false;
+        } 
 
         // Timer countdown
         if (IsCasting)

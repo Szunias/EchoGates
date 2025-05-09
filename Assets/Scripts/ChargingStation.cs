b08b80f974bdfa781c4afd99742d58e9e7862c40
FleurLievens <fleur.lievens1@gmail.com>
2025-05-09 10:59:38 +0200

    ChargingStation script is now linked to EnergyController script

--- Assets/Scripts/ChargingStation.cs
+++ Assets/Scripts/ChargingStation.cs
@@ -1,21 +1,107 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System.Collections;
 
 public class ChargingStation : MonoBehaviour
 {
+    [SerializeField] private EnergyController playerEnergy;
+    [SerializeField] private float energyReward = 25f;
+    [SerializeField] public float chargeAmount = 100f; 
+    [SerializeField] private float chargeSpeed = 10f;
+
     private bool playerInRange = false;
-    private EnergyController playerEnergy;
+    private GameObject placedObject;
+    private bool isObjectPlaced = false;
 
-    [SerializeField] private float rechargeAmount = 100f;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject chargeablePrefab;
+    [SerializeField] private InputActionReference interactAction;
 
-    void Update()
+    private void OnEnable()
     {
-        if (playerInRange && Input.GetKeyDown(KeyCode.Q))
+        interactAction.action.performed += OnInteract;
+        interactAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        interactAction.action.performed -= OnInteract;
+        interactAction.action.Disable();
+    }
+
+    private void OnInteract(InputAction.CallbackContext context)
+    {
+        if (playerInRange)
         {
-            if (playerEnergy != null)
-            {
-                playerEnergy.AddEnergy(rechargeAmount);
-                Debug.Log("Energy fully recharged!");
-            }
+            HandleInteraction();
+        }
+    }
+
+    private void HandleInteraction()
+    {
+        if (placedObject == null)
+        {
+            SpawnObject();
+        }
+        else if (isObjectPlaced && playerEnergy.Percent >= playerEnergy.MaxEnergy)
+        {
+            RetrieveObject();
+        }
+        else
+        {
+            Debug.Log("Charging in progress...");
+        }
+    }
+
+    private void SpawnObject()
+    {
+        if ( playerEnergy.Percent >= playerEnergy.MaxEnergy)
+        {
+            Debug.Log("Energy already full. No need to charge. ");
+            return;
+        }
+
+        placedObject = Instantiate(chargeablePrefab, spawnPoint.position, spawnPoint.rotation);
+        isObjectPlaced = true;
+
+        Debug.Log("Placed object: " + placedObject);
+        Debug.Log("Object placed on charging station.");
+
+        StartCoroutine(ChargeObject());
+    }
+
+    private IEnumerator ChargeObject()
+    {
+        while (playerEnergy.Percent < playerEnergy.MaxEnergy && isObjectPlaced)
+        {
+            playerEnergy.AddEnergy (Time.deltaTime * chargeSpeed);
+            yield return null;
+        }
+
+        if (playerEnergy.Percent >= playerEnergy.MaxEnergy)
+        {
+            Debug.Log("Charging complete.");
+        }
+    }
+
+    private void RetrieveObject()
+    {
+        if (placedObject != null && playerEnergy.Percent >= chargeAmount)
+        {
+            Debug.Log("Placed object: " + placedObject);
+            Debug.Log("Destroying placed object...");
+
+            Destroy(placedObject);
+
+            placedObject = null;
+            isObjectPlaced = false;
+            playerEnergy.AddEnergy(energyReward);
+            Debug.Log("Energy added to player. ");
+        }
+        else
+        {
+            Debug.Log("Object is not fully charged.");
         }
     }
 
@@ -24,8 +110,7 @@ public class ChargingStation : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = true;
-            playerEnergy = other.GetComponent<EnergyController>();
-            Debug.Log("Player in range of charging station.");
+            Debug.Log("Player is in range to interact.");
         }
     }
 
@@ -34,11 +119,11 @@ public class ChargingStation : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
-            playerEnergy = null;
-            Debug.Log("Player left charging station.");
+            Debug.Log("Player is out of range.");
         }
     }
 }
+
 
 
 

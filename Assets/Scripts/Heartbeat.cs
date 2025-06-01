9a0a8c454ec783b007387ed4fb36558f8fe3ae8c
Luca Calmera <acacianbench@gmail.com>
2025-06-01 10:09:45 +0200

    Add even more SFX
    - Heartbeat that speeds up when the player gets closer to the spdier
    - Spider makes creature noises

---
+++ Assets/Scripts/Heartbeat.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+public class Heartbeat : MonoBehaviour
+{
+    private AudioSource source;
+    private Vector3 previousSpiderPosition;
+    private Vector3 previousPlayerPosition;
+
+    [SerializeField] public GameObject spider;
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 currentSpiderPosition = spider.transform.position;
+        float distanceFromSpider = Vector3.Distance(currentPosition, currentSpiderPosition);
+        source.pitch = Mathf.Lerp(source.pitch, GetPitch(distanceFromSpider), Time.deltaTime * 5f);
+        previousPlayerPosition = currentPosition;
+        previousSpiderPosition = currentSpiderPosition;
+    }
+
+    private float GetPitch(float distance)
+    {
+        float t = 1f - Mathf.Clamp01(distance / 40f);
+        return Mathf.Lerp(0.6f, 2f, t);
+    }
+}
